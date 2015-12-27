d43d6eb9d90486d0a2229c735ec235da8b38a5ae
Sebastian Tschan <mail@blueimp.net>
2015-12-27 21:12:26 +0100

    Remove obsolete jQuery dependency from the demo.

--- js/demo.js
+++ js/demo.js
@@ -9,40 +9,61 @@
  * http://www.opensource.org/licenses/MIT
  */
 
-/*global window, $ */
+/* global document, tmpl */
 
-$(function () {
+(function () {
     'use strict';
 
-    var render = function (event) {
-            event.preventDefault();
-            var result;
-            try {
-                result = window.tmpl(
-                    $('#template').val(),
-                    $.parseJSON($('#data').val())
-                );
-            } catch (e) {
-                result = window.tmpl('tmpl-error', e);
-            }
-            $('#result').html(result);
-        },
-        init = function (event) {
-            if (event) {
-                event.preventDefault();
-            }
-            $('#template').val($.trim($('#tmpl-demo').html()));
-            $('#data').val($.trim($('#tmpl-data').html()));
-            $('#result').empty();
-        },
-        error = function (e) {
-            $('#result').html(
-                window.tmpl('tmpl-error', e.originalEvent.message)
+    var templateInput = document.getElementById('template');
+    var dataInput = document.getElementById('data');
+    var resultNode = document.getElementById('result');
+    var templateDemoNode = document.getElementById('tmpl-demo');
+    var templateDataNode = document.getElementById('tmpl-data');
+
+    function renderError(title, error) {
+        resultNode.innerHTML = tmpl(
+            'tmpl-error',
+            {title: title, error: error}
+        );
+    }
+
+    function render(event) {
+        event.preventDefault();
+        var data;
+        try {
+            data = JSON.parse(dataInput.value);
+        } catch (e) {
+            renderError('JSON parsing failed', e);
+            return;
+        }
+        try {
+            resultNode.innerHTML = tmpl(
+                templateInput.value,
+                data
             );
-        };
-    $(window).on('error', error);
-    $('#render').on('click', render);
-    $('#reset').on('click', init);
+        } catch (e) {
+            renderError('Template rendering failed', e);
+        }
+    }
+
+    function empty(node) {
+        while (node.lastChild) {
+            node.removeChild(node.lastChild);
+        }
+    }
+
+    function init(event) {
+        if (event) {
+            event.preventDefault();
+        }
+        templateInput.value = templateDemoNode.innerHTML.trim();
+        dataInput.value = templateDataNode.innerHTML.trim();
+        empty(resultNode);
+    }
+
+    document.getElementById('render').addEventListener('click', render);
+    document.getElementById('reset').addEventListener('click', init);
+
     init();
 
-});
+}());
