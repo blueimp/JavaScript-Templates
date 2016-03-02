4cc374d49b2e51c5b375b9d107c3358e5ca6140c
Adrien <adrien.vetterli@gmail.com>
2016-03-02 17:00:13 +0200

    Update compile.js

--- js/compile.js
+++ js/compile.js
@@ -77,7 +77,7 @@
   }
   // Combine the generated functions as cache of the minimal runtime:
   code = runtime.replace('{}', '{' + list.join(',') + '}')
-  // Write the cmpiled code to an arbitrary outfile:
+  // Write the compiled code to an arbitrary outfile:
     fs.writeFile(path.join(__dirname, 'compiled_template.js'), code, function(err) {
     if(err) {
 	return console.log(err);
